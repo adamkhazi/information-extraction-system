b273550b9202b463f2b2c43cef89f2318f347426
Adam Khazi <adamk4@gmail.com>
2016-12-23 21:21:25 +0700

    nltk tokenization on a single string

--- text_extractor.py
+++ text_extractor.py
@@ -1,5 +1,19 @@
 import tika
+import nltk
+
 from tika import parser
+from nltk.tokenize import word_tokenize
+
 parsed = parser.from_file('test.pdf')
-print(parsed["metadata"])
-print(parsed["content"])
+#print(parsed["metadata"])
+#print(parsed["content"])
+
+# type string
+content = parsed["content"]
+
+print("tokeninzing")
+
+tknzr = word_tokenize(content)
+#tokenized_content = tknzr.tokenize(content)
+print(tknzr)
+
