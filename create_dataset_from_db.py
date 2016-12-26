0e02ddd0abe58123247b329d8d37c98dd71c0ba3
Adam Khazi <adamk4@gmail.com>
2016-12-26 19:44:42 +0700

    cleaned up create from db code and better print statements

--- create_dataset_from_db.py
+++ create_dataset_from_db.py
@@ -6,6 +6,9 @@ import os
 from nltk.tokenize import word_tokenize
 from numpy import array
 from ast import literal_eval
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
 from database_connection import DatabaseConnection
 
 conn = DatabaseConnection().connect()
@@ -15,31 +18,40 @@ cursor = conn.cursor()
 #cursor.execute("SELECT TOP 50 cn_fname, cn_lname, cn_resume FROM tblCandidate WHERE cn_fname IS NOT NULL AND DATALENGTH(cn_fname)>2 AND cn_lname IS NOT NULL AND DATALENGTH(cn_lname)>2 AND cn_resume LIKE '%[a-z0-9]%' AND DATALENGTH(cn_resume)>10000 AND cn_res=0;")
 
 #random
-cursor.execute("SELECT TOP 10 cn_fname, cn_lname, cn_resume FROM tblCandidate WHERE cn_fname IS NOT NULL AND DATALENGTH(cn_fname)>2 AND cn_lname IS NOT NULL AND DATALENGTH(cn_lname)>2 AND cn_resume LIKE '%[a-z0-9]%' AND DATALENGTH(cn_resume)>10000 AND cn_res=0 ORDER BY NEWID();")
+cursor.execute("SELECT TOP 200 cn_fname, cn_lname, cn_resume FROM tblCandidate WHERE cn_fname IS NOT NULL AND DATALENGTH(cn_fname)>2 AND cn_lname IS NOT NULL AND DATALENGTH(cn_lname)>2 AND cn_resume LIKE '%[a-z0-9]%' AND DATALENGTH(cn_resume)>10000 AND cn_res=0 ORDER BY NEWID();")
 
 row = cursor.fetchone()
 
 tw_temp = []
 tw_tag_temp = []
+pers_tag_count = 0
 while row:
-    temp = word_tokenize(row[2])
-    print("testing for: " + str(row[0]) + " or " + str(row[1]))
+    rtokenizer = RegexpTokenizer(r'\w+')
+    tokens = rtokenizer.tokenize(row[2])
+    filtered_words = [w for w in tokens if not w in stopwords.words('english')]
+
+    temp = word_tokenize(" ".join(filtered_words))
+    print("tagging: " + str(row[0]) + " and " + str(row[1]))
     for idx, n in enumerate(temp):
-        if ((len(n) >= len(str(row[0]))) and (n.lower() in str(row[0]).lower())) or ((len(n) >= len(str(row[1]))) and (str(row[1]).lower() in n.lower())):
+        names = word_tokenize((str(row[0]) + str(row[1])).lower())
+        
+        if any(n.lower() == s for s in names):
             tw_tag_temp.append("PERS")
+            pers_tag_count +=1
         else: 
             tw_tag_temp.append("O")
     tw_temp = tw_temp + temp
     row = cursor.fetchone()
     
-print(len(tw_temp))
-print(len(tw_tag_temp))
+print("tokens: " + str(len(tw_temp)))
+print("tags: " + str(len(tw_tag_temp)))
+print("PERS tag count: " + str(pers_tag_count))
 
 dataset_folder = "db_generated_datasets"
 training_file = "ner_training_data.txt"
 test_file = "ner_test_data.txt"
 
-save_file = dataset_folder + "/" + training_file
+save_file = dataset_folder + "/" + test_file
 try:
     os.remove(save_file)
 except OSError:
@@ -47,3 +59,5 @@ except OSError:
 with open (save_file,'a') as proc_seqf:
     for a, am in zip(tw_temp, tw_tag_temp):
         proc_seqf.write("{}\t{}\n".format(a, am))
+
+print("saved: " + save_file)
