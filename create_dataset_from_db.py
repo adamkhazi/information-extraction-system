4d1382436d5509b48f623991c8289dcef97e41ba
Adam Khazi <adamk4@gmail.com>
2017-01-02 11:40:57 +0700

    better data structure and writing to file that shows document seperation

--- create_dataset_from_db.py
+++ create_dataset_from_db.py
@@ -17,37 +17,43 @@ cursor = conn.cursor()
 
 sql_query_ordered = "SELECT TOP 50 cn_fname, cn_lname, cn_resume FROM tblCandidate WHERE cn_fname IS NOT NULL AND DATALENGTH(cn_fname)>2 AND cn_lname IS NOT NULL AND DATALENGTH(cn_lname)>2 AND cn_resume LIKE '%[a-z0-9]%' AND DATALENGTH(cn_resume)>10000 AND cn_res=0;"
 
-sql_query_random = "SELECT TOP 100 cn_fname, cn_lname, cn_resume FROM tblCandidate WHERE cn_fname IS NOT NULL AND DATALENGTH(cn_fname)>2 AND cn_lname IS NOT NULL AND DATALENGTH(cn_lname)>2 AND cn_resume LIKE '%[a-z0-9]%' AND DATALENGTH(cn_resume)>17000 AND cn_res=0 ORDER BY NEWID();"
+sql_query_random = "SELECT TOP 1000 cn_fname, cn_lname, cn_resume FROM tblCandidate WHERE cn_fname IS NOT NULL AND DATALENGTH(cn_fname)>2 AND cn_lname IS NOT NULL AND DATALENGTH(cn_lname)>2 AND cn_resume LIKE '%[a-z0-9]%' AND DATALENGTH(cn_resume)>17000 AND cn_res=0 ORDER BY NEWID();"
 
 cursor.execute(sql_query_random)
 
 row = cursor.fetchone()
 
-tw_temp = []
-tw_tag_temp = []
+tagged_tokens = []
+pers_count = 0
 
 while row:
     rtokenizer = RegexpTokenizer(r'\w+')
     tokens = rtokenizer.tokenize(row[2])
     filtered_words = [w for w in tokens if not w in stopwords.words('english')]
 
-    temp = word_tokenize(" ".join(filtered_words))
-    print("tagging: " + str(row[0]) + " and " + str(row[1]))
-    for idx, n in enumerate(temp):
+    temp_doc_tokens = word_tokenize(" ".join(filtered_words))
+    temp_pos_tags = nltk.pos_tag(temp_doc_tokens)
+    
+    print("Tagging document: " + str(row[0]) + " _ " + str(row[1]))
+
+    for idx, n in enumerate(temp_doc_tokens):
         names = rtokenizer.tokenize((str(row[0]) + " " + str(row[1])).lower())
         
         if any(n.lower() == s for s in names):
-            tw_tag_temp.append("PERS")
+            #replace word with tagged tuple
+            temp_doc_tokens[idx] = (temp_doc_tokens[idx], temp_pos_tags[idx][1], "PERS")
+            pers_count += 1
         else: 
-            tw_tag_temp.append("O")
-    tw_temp = tw_temp + temp
-    row = cursor.fetchone()
-    
-print("tokens: " + str(len(tw_temp)))
-print("tags: " + str(len(tw_tag_temp)))
-print("PERS tag count: " + str(len(tw_tag_temp)))
+            temp_doc_tokens[idx] = (temp_doc_tokens[idx], temp_pos_tags[idx][1], "O")
 
-tw_pos_temp = nltk.pos_tag(tw_temp)
+    row = cursor.fetchone()
+    # each list within tagged_tokens is a document
+    tagged_tokens.append(temp_doc_tokens)
+    
+print("documents: " + str(len(tagged_tokens)))
+print("tokens: " + str(sum([len(doc) for doc in tagged_tokens])))
+# TODO PERS COUNT
+#print("PERS count: " + str(sum(a in doc for doc in tagged_tokens)))
 
 dataset_folder = "db_generated_datasets"
 ner_file = "ner_dataset.txt"
@@ -58,7 +64,9 @@ try:
 except OSError:
     pass
 with io.open (save_file,'a', encoding='utf-8') as proc_seqf:
-    for a, pos, am in zip(tw_temp, tw_pos_temp, tw_tag_temp):
-        proc_seqf.write("{}\t{}\t{}\n".format(a ,pos[1], am))
+    for idx, item in enumerate(tagged_tokens):
+        for token in enumerate(tagged_tokens[idx]):
+            proc_seqf.write("{}\t{}\t{}\n".format(token[1][0], token[1][1], token[1][2]))
+        proc_seqf.write("\n")
 
 print("saved to: " + save_file)
--- crfsuite.py
+++ crfsuite.py
@@ -14,15 +14,21 @@ test_sents = []
 total_sents = []
 
 print("reading in dataset")
-with io.open('db_generated_datasets/ner_training_data.txt', 'r',
-encoding='utf-8') as tsvin:
+with io.open('db_generated_datasets/ner_dataset.txt', 'r', encoding='utf-8') as tsvin:
+    temp_doc_tokens = []
     tsvin = csv.reader(tsvin, delimiter='\t')
     for row in tsvin:
-        total_sents.append((row[0], row[1], row[2])) #append tuples
+        if not row:
+            total_sents.append(temp_doc_tokens)
+            temp_doc_tokens = []
+        else: 
+            temp_doc_tokens.append((row[0], row[1], row[2])) #append tuples
+
+print("Read " + str(len(total_sents)) + " documents")
 
 split_point = math.ceil(len(total_sents) * 0.7)
-train_sents = [list(total_sents[0:split_point])]
-test_sents = [list(total_sents[split_point+1:])]
+train_sents = total_sents[0:split_point]
+test_sents = total_sents[split_point+1:]
 print("done reading and splitting dataset")
 
 #nltk.corpus.conll2002.fileids()
@@ -46,33 +52,33 @@ def word2features(sent, i):
         'postag=' + postag,
         'postag[:2]=' + postag[:2],
     ]
-    """
+    
     if i > 0:
         word1 = sent[i-1][0]
-#        postag1 = sent[i-1][1]
+        postag1 = sent[i-1][1]
         features.extend([
             '-1:word.lower=' + word1.lower(),
             '-1:word.istitle=%s' % word1.istitle(),
             '-1:word.isupper=%s' % word1.isupper(),
-#            '-1:postag=' + postag1,
-#            '-1:postag[:2]=' + postag1[:2],
+            '-1:postag=' + postag1,
+            '-1:postag[:2]=' + postag1[:2],
         ])
     else:
         features.append('BOS')
         
     if i < len(sent)-1:
         word1 = sent[i+1][0]
-#        postag1 = sent[i+1][1]
+        postag1 = sent[i+1][1]
         features.extend([
             '+1:word.lower=' + word1.lower(),
             '+1:word.istitle=%s' % word1.istitle(),
             '+1:word.isupper=%s' % word1.isupper(),
-#            '+1:postag=' + postag1,
-#            '+1:postag[:2]=' + postag1[:2],
+            '+1:postag=' + postag1,
+            '+1:postag[:2]=' + postag1[:2],
         ])
     else:
         features.append('EOS')
-    """
+
     return features
 
 def sent2features(sent):
@@ -104,11 +110,10 @@ for xseq, yseq in zip(X_train, y_train):
     trainer.append(xseq, yseq)
 print("pycrfsuite Trainer has data")
 
-trainer.select("l2sgd")
 trainer.set_params({
-    #'c1': 1.0,   # coefficient for L1 penalty
+    'c1': 1.0,   # coefficient for L1 penalty
     'c2': 1,  # coefficient for L2 penalty
-    'max_iterations': 300,  # stop earlier
+    'max_iterations': 400,  # stop earlier
 
     # include transitions that are possible, but not observed
     'feature.possible_transitions': True
@@ -137,16 +142,16 @@ print(' '.join(sent2tokens(example_sent)), end='\n\n')
 print("Predicted:", ' '.join(tagger.tag(sent2features(example_sent))))
 print("Correct:  ", ' '.join(sent2labels(example_sent)))
 
-
 #evaluate model
-"""
 def bio_classification_report(y_true, y_pred):
 
+    """
     Classification report for a list of BIO-encoded sequences.
     It computes token-level metrics and discards "O" labels.
     
     Note that it requires scikit-learn 0.15+ (or a version from github master)
     to calculate averages properly!
+    """
 
     lb = LabelBinarizer()
     y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
@@ -166,4 +171,3 @@ def bio_classification_report(y_true, y_pred):
 y_pred = [tagger.tag(xseq) for xseq in X_test]
 
 print(bio_classification_report(y_test, y_pred))
-"""
