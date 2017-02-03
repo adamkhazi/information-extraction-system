9cbee0cf6526da32ae65707108dbc8a7682a734a
Adam Khazi <adamk4@gmail.com>
2017-02-03 15:50:49 +0000

    logging added to extractor indicating progress

--- extractor.py
+++ extractor.py
@@ -9,6 +9,10 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
+from logger import Logger
+
+# This class extracts content from resume files of various formats.
+# Also it extracts content from XML files and turns them into Python objects.
 class Extractor:
     __dataset_raw_data_folder = "dataset_raw_data"
     __file_path_seperator = "/"
@@ -19,6 +23,10 @@ class Extractor:
     __file_ext_doc = ".doc"
     __file_ext_docx = ".docx"
     __file_ext_msg = ".msg"
+
+    def __init__(self):
+        self.logger = Logger()
+        self.logger.println("extractor created")
 
     def get_edu_majors(self, label_set):
         try:
@@ -98,6 +106,7 @@ class Extractor:
                 counter += 1
                 if counter == nr_of_files and nr_of_files != -1:
                     break
+        self.logger.println("read %s file names" % counter)
 
     def __read_resume_content(self):
         # files share an index
@@ -105,6 +114,7 @@ class Extractor:
         file_metadata = []
 
         for idx, filename in enumerate(self.dataset_filenames):
+            self.logger.println("sending resume %s/%s to tika" % (idx, len(self.dataset_filenames)-1) )
             # append filename + ext to path
             filepath = self.__dataset_raw_data_folder + self.__file_path_seperator + filename[0] + filename[1]
             extracted_information = parser.from_file(filepath)
@@ -113,6 +123,7 @@ class Extractor:
 
         self.resume_content = file_content
         self.resume_metadata = file_metadata
+        self.logger.println("read content from %s resume files" % len(self.resume_content))
 
     def __read_resume_labels(self):
         resume_labels = []
@@ -121,12 +132,14 @@ class Extractor:
             xml_file = untangle.parse(filepath)
             resume_labels.append(xml_file)
         self.resume_labels = resume_labels
+        self.logger.println("read labels from %s xml files" % len(self.resume_labels))
 
     def __remove_empty_resumes(self):
         for idx, file_content in enumerate(self.resume_content):
             if file_content is None:
                 del self.dataset_filenames[idx]
                 del self.resume_content[idx]
+        self.logger.println("removed empty resume files and total file count is at %s" % len(self.resume_content))
 
     def read_raw_files(self, nr_of_docs):
         self.__populate_file_names(nr_of_docs)
@@ -135,6 +148,8 @@ class Extractor:
         self.__read_resume_labels()
         return self.resume_content, self.resume_labels
 
+    # method is required when parsing and tagging a single file, mostly for
+    # demonstration
     def read_resume_content(self, filepath):
         extracted_information = parser.from_file(filepath)
         docs = []
