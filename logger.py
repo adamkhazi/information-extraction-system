0ce3f7914703512d0e1b1033ea375a5dc10ea309
Adam Khazi <adamk4@gmail.com>
2017-02-03 14:43:07 +0000

    logger for log output with timestamp

--- logger.py
+++ logger.py
@@ -1,13 +1,18 @@
+import logging
+
 class Logger:
     __carriage_return = "\r"
     __new_line = "\n"
     __empty_string = ""
 
+    def __init__(self):
+        self.__logging.basicConfig(format='[%(asctime)s] : %(levelname)s : %(message)s', level=logging.INFO)
+
     def new_line(self):
         print(self.__new_line)
 
     def println(self, msg):
-        print(msg)
+        self.__logging.info(msg)
 
     def print(self, msg):
         output = self.__carriage_return + msg
