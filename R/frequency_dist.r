052d01e998aee56b6ced99e3c59fd99225c64b02
Adam Khazi <adamk4@gmail.com>
2017-03-09 20:52:31 +0000

    R freq tag dist code added

--- R/frequency_dist.r
+++ R/frequency_dist.r
@@ -7,10 +7,14 @@ for (name in files) {
 	dataset <- append(dataset, list(temp_dataset))
 }
 
-dataset =rbindlist(dataset)
+dataset = rbindlist(dataset)
 
 pal12 = brewer.pal(12, "Paired")
 colors = colorRampPalette(pal12)(40) 
 
+# for tag freq distribution log scale
+barplot(table(dataset$V4), col=terrain.colors(dataset$V4), log="y", cex.names=0.55, cex.axis = 0.45, xlab="Named-Entity Tags", ylab="Frequency (Log)")
+
+filtered = dataset[V4!="O"]
 # filtered has the named entity tags "O" removed from it 
 barplot(table(filtered), col=colors, legend=levels(filtered$V2), cex.names=0.8, args.legend = list(x ='topright', bty='n', inset=c(0,0)), xlim=c(0, ncol(filtered) +9), ylab="Frequency", xlab="Named Entity Tag")
