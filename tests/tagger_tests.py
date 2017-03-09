bf7d9bbf7936d0a9a836a0e94a5fe0cd19c014e1
Adam Khazi <adamk4@gmail.com>
2017-03-09 12:45:07 +0000

    tests for tagger pos tagging dimensions and no alteration

--- tests/tagger_tests.py
+++ tests/tagger_tests.py
@@ -8,9 +8,8 @@ class TaggerTests(unittest.TestCase):
 
     ### POS ###
     def pos_tag_get_results(self, tagger):
-        input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""),
-            ("test", "", "", "")], [("this", "", "", ""), ("is", "", "", ""),
-                ("a", "", "", ""), ("test", "", "", "")]]
+        input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")],
+                [("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")]]
         output = tagger.pos_tag(input)
         return input, output
 
@@ -20,7 +19,70 @@ class TaggerTests(unittest.TestCase):
         input, output = self.pos_tag_get_results(tagger)
         self.assertEqual(len(input), len(output))
 
-    ### MATCH LABELS - WINDOW MATCHING  ###
+    # check if same nr of tokens returned
+    def test_pos_tag_same_nr_tokens(self):
+        tagger = Tagger()
+        input, output = self.pos_tag_get_results(tagger)
+
+        input_nr_tuples = [len(line) for line in input]
+        input_nr_tuples = sum(input_nr_tuples)
+
+        output_nr_tuples = [len(line) for line in output]
+        output_nr_tuples = sum(output_nr_tuples)
+        self.assertEqual(input_nr_tuples, output_nr_tuples)
+
+    # check if all tuples contain 4 slots
+    def test_pos_tag_4_slots_each_tuple(self):
+        tagger = Tagger()
+        input, output = self.pos_tag_get_results(tagger)
+
+        input_nr_tuple_slots = [1 for line in input for tuple in line if len(tuple) == 4]
+        input_nr_tuple_slots = sum(input_nr_tuple_slots)
+
+        output_nr_tuple_slots = [1 for line in output for tuple in line if len(tuple) == 4]
+        output_nr_tuple_slots = sum(output_nr_tuple_slots)
+        self.assertEqual(input_nr_tuple_slots, output_nr_tuple_slots)
+
+    # check if same token strs returned in first slot of tuple 
+    def test_pos_tag_same_token_strs_returned(self):
+        tagger = Tagger()
+        input, output = self.pos_tag_get_results(tagger)
+
+        same_tokens_returned = True
+        for line_idx, line in enumerate(output):
+            for tuple_idx, tuple in enumerate(line):
+                if tuple[0] != input[line_idx][tuple_idx][0]:
+                    same_tokens_returned = False
+
+        self.assertEqual(same_tokens_returned, True)
+
+    # check if same token strs returned in first slot of tuple 
+    def test_pos_tag_sner_labels_not_altered(self):
+        tagger = Tagger()
+        input, output = self.pos_tag_get_results(tagger)
+
+        same_pos_labels_returned = True
+        for line_idx, line in enumerate(output):
+            for tuple_idx, tuple in enumerate(line):
+                if tuple[3] != input[line_idx][tuple_idx][3]:
+                    same_pos_labels_returned = False
+
+        self.assertEqual(same_pos_labels_returned, True)
+
+    # check if same token strs returned in first slot of tuple 
+    def test_pos_tag_nonlocalner_labels_not_altered(self):
+        tagger = Tagger()
+        input, output = self.pos_tag_get_results(tagger)
+
+        same_nlner_labels_returned = True
+        for line_idx, line in enumerate(output):
+            for tuple_idx, tuple in enumerate(line):
+                if tuple[2] != input[line_idx][tuple_idx][2]:
+                    same_nlner_labels_returned = False
+
+        self.assertEqual(same_nlner_labels_returned, True)
+
+    ####################### MATCH LABELS - WINDOW MATCHING  #######################
     def match_label_get_results(self, tagger):
         input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""),
             ("test", "", "", "")], [("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")]]
@@ -94,7 +156,7 @@ class TaggerTests(unittest.TestCase):
         same_nlner_labels_returned = True
         for line_idx, line in enumerate(output):
             for tuple_idx, tuple in enumerate(line):
-                if tuple[3] != input[line_idx][tuple_idx][3]:
+                if tuple[2] != input[line_idx][tuple_idx][2]:
                     same_nlner_labels_returned = False
 
         self.assertEqual(same_nlner_labels_returned, True)
