930959f78628087f3de12eed1ee50685aa82f468
Adam Khazi <adamk4@gmail.com>
2017-03-09 20:54:28 +0000

    tagger tests nonlocal ner

--- tests/tagger_tests.py
+++ tests/tagger_tests.py
@@ -6,7 +6,8 @@ from tagger import Tagger
 #python3 -m unittest tests.tagger_tests
 class TaggerTests(unittest.TestCase):
 
-    ### POS ###
+    ####################### POS #######################
+    # One document at a time
     def pos_tag_get_results(self, tagger):
         input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")],
                 [("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")]]
@@ -83,9 +84,10 @@ class TaggerTests(unittest.TestCase):
         self.assertEqual(same_nlner_labels_returned, True)
 
     ####################### MATCH LABELS - WINDOW MATCHING  #######################
+    # One document at a time
     def match_label_get_results(self, tagger):
-        input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""),
-            ("test", "", "", "")], [("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")]]
+        input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")],
+                [("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")]]
         input_label = "Not a match"
         input_match_tag = "match"
         output = tagger.match_label(input, input_label, input_match_tag)
@@ -183,16 +185,51 @@ class TaggerTests(unittest.TestCase):
                         correct_iob = False
         self.assertEqual(correct_iob, True)
 
-"""
-    ### NONLOCAL NER ###
-    def test_nonlocal_ner_tag(self):
+    ####################### NONLOCAL NER #######################
+    # More than one document
+    def nonlocal_ner_get_results(self, tagger):
+        input =  [[("this", "", "", "d"), ("is", "", "", "d"), ("a", "", "", "d"), ("test", "", "", "d")],
+                [("this", "", "", "d"), ("is", "", "", "d"), ("a", "", "", "d"), ("test", "", "", "d")]]
+        output = tagger.nonlocal_ner_tag([input])
+        return input, output
+
+    def test_nonlocal_ner_nr_lines(self):
         tagger = Tagger()
+        input, output = self.nonlocal_ner_get_results(tagger)
+        self.assertEqual(len(input), len(output))
 
-        input =  [[("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")],
-            [("this", "", "", ""), ("is", "", "", ""), ("a", "", "", ""), ("test", "", "", "")]]
+    def test_nonlocal_ner_same_nr_tokens(self):
+        tagger = Tagger()
+        input, output = self.nonlocal_ner_get_results(tagger)
 
-        correct_output = [[("this", "O", "", ""), ("is", "O", "", ""), ("a", "O", "", ""), ("test", "O", "", "")],
-                [("this", "O", "", ""), ("is", "O", "", ""), ("a", "O", "", ""), ("test", "O", "", "")]]
+        input_nr_tuples = [len(line) for line in input]
+        input_nr_tuples = sum(input_nr_tuples)
 
-        output = tagger.nonlocal_ner_tag(input)
-"""
+        output_nr_tuples = [len(line) for line in output]
+        output_nr_tuples = sum(output_nr_tuples)
+        self.assertEqual(input_nr_tuples, output_nr_tuples)
+
+    # check if all tuples contain 4 slots
+    def test_nonlocal_ner_4_slots_each_tuple(self):
+        tagger = Tagger()
+        input, output = self.nonlocal_ner_get_results(tagger)
+
+        input_nr_tuple_slots = [1 for line in input for tuple in line if len(tuple) == 4]
+        input_nr_tuple_slots = sum(input_nr_tuple_slots)
+
+        output_nr_tuple_slots = [1 for line in output for tuple in line if len(tuple) == 4]
+        output_nr_tuple_slots = sum(output_nr_tuple_slots)
+        self.assertEqual(input_nr_tuple_slots, output_nr_tuple_slots)
+
+    # check if same token strs returned in first slot of tuple 
+    def test_nonlocal_ner_same_token_strs_returned(self):
+        tagger = Tagger()
+        input, output = self.nonlocal_ner_get_results(tagger)
+
+        same_tokens_returned = True
+        for line_idx, line in enumerate(output):
+            for tuple_idx, tuple in enumerate(line):
+                if tuple[0] != input[line_idx][tuple_idx][0]:
+                    same_tokens_returned = False
+
+        self.assertEqual(same_tokens_returned, True)
