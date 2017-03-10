09baf56c9aeecc1ff843949a47b899934bc4c9d8
Adam Khazi <adamk4@gmail.com>
2017-03-10 00:11:58 +0000

    R word cloud code

--- R/frequency_dist.r
+++ R/frequency_dist.r
@@ -18,3 +18,9 @@ barplot(table(dataset$V4), col=terrain.colors(dataset$V4), log="y", cex.names=0.
 filtered = dataset[V4!="O"]
 # filtered has the named entity tags "O" removed from it 
 barplot(table(filtered), col=colors, legend=levels(filtered$V2), cex.names=0.8, args.legend = list(x ='topright', bty='n', inset=c(0,0)), xlim=c(0, ncol(filtered) +9), ylab="Frequency", xlab="Named Entity Tag")
+
+# word cloud - word frequency
+freqs = count(w)
+wordcloud(words = freqs$x.Var1, freq = freqs$x.Freq, min.freq = 1,
+	  max.words=500, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
