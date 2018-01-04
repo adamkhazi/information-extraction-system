90fa9dc90d7e061a7efbc8184c0ca10aec6576ee
Adam Khazi <adamkhazi@users.noreply.github.com>
2018-01-04 14:34:24 +0700

    delete parser api call

--- zylon_legacy_parser.py
+++
@@ -1,5 +0,0 @@
-url = "http://rcparser.azurewebsites.net/ParserService.asmx/ParseAjax"
-params = {"filename": "test_cv.docx",
-        "fileformat": "docx",
-        "UploadedFile": ("test_cv.docx", open("test_cv.docx", "rb")),
-        }
